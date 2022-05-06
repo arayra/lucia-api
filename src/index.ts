6185e397e7b4236c6cdaf84ed6d04878ffa8f52a
irlanvila <irlanvila@outlook.com>
2022-05-06 07:40:16 +0700

    create connection to mdb

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,18 @@
 import * as express from "express";
 import * as cors from "cors";
 import * as multer from "multer";
+import * as mongoose from "mongoose";
 import "dotenv/config";
 
 const app = express();
 const upload = multer();
 const PORT = process.env.PORT;
+const uri = process.env.MDB_URI;
+
+mongoose
+  .connect(uri)
+  .then(() => console.log("MDB Connected Successfully!"))
+  .catch((err) => console.log(err));
 
 app.use(cors());
 
